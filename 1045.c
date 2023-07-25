b76ac307346e774f574d021fa12ac9b6edd42954
karinysis <leeyoungjae@pusan.ac.kr>
2023-07-26 06:04:12 +0900

    [문제] 1045번

---
+++ 1045.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+        long long int a,b;
+	scanf("%lld %lld",&a,&b);
+	printf("%lld\n%lld\n%lld\n%lld\n%lld\n%.2lf",a+b,a-b,a*b,a/b,a%b,(float)a/b);
+        return 0;
+}
